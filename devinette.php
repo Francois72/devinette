8a5fbc2995eacfa26c382dd185b2c059f4377150
François <francoisbergerot@orange.fr>
2018-12-27 10:23:13 +0100

    Création du fichier devinette.php

---
+++ devinette.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Devinette</title>
+    </head>
+ 
+    <body>
+	
+	<form action="cible.php" method="post">
+		<p><label>Qu'est-ce qui est jaune et qui attend ? <input type="text" id="reponse"  name="reponse" /></label></p>
+		<input type="submit" value="valider"/>		
+	</form>		
+   
+    </body>
+</html>
